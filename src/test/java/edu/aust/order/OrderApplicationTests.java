3dd6a2e909d2f98f5a19943427a28fffb920c6db
Shafaat S Giasuddin <s.giasuddin@cimsolutions.nl>
2025-08-19 00:28:18 +0600

    Order System

---
+++ src/test/java/edu/aust/order/OrderApplicationTests.java
@@ -0,0 +1,12 @@
+package edu.aust.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
